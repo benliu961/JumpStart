57edc98c25475df0ac7312965eb30d03ac18ce8a
Eugene Liu <eugeneliu@Eugenes-MacBook-Air-2.local>
2024-04-22 17:33:06 -0400

    new updates to JRSL.py

--- jsrl.py
+++ jsrl.py
@@ -1,4 +1,5 @@
 import gym
+import random as rand
 
 '''
 Classes and function necessary for JSRL. 
@@ -30,33 +31,65 @@ class JSRL():
         return self.guide_policy
 
 
-def __random_switch() -> list:
+def __random_switch(horizon: int, n: int) -> list:
     '''Helper function to determine guide steps based on random switching'''
-    pass
 
-def __curriculum() -> list:
+    guide_steps = []
+
+    for i in range(n):
+        guide_steps.append(rand.randint(horizon))
+
+    return guide_steps
+    
+
+def __curriculum(horizon: int, n:int) -> list:
     '''Helper function to determine guide steps based on curriculum'''
+
+    constant = horizon/n
+    current = horizon
+
+    guide_steps = []
+    while current != 0:
+        guide_steps.append(current - constant)
+        current = current - constant
+    
+    return guide_steps
+
+
+def __evaluation(policy):
+    '''Helper function that evaluates the combined policy'''
     pass
-        
 
+def update(policy):
+    '''Helper function that updates the exploration policy based on learned trajectory'''
+    pass
 
-def trainer(object: JSRL, horizon: int, environment, random_switch=False):
+def trainer(object: JSRL, horizon: int, n:int, environment, random_switch=False):
 
-    guide_policy = JSRL.guide_policy_return()
-    exploration_policy = JSRL.exploration_policy_return()
 
     # initializing guide steps based on either random_switch or curriculum
     if random_switch:
-        guide_steps = __random_switch(horizon)
+        guide_steps = __random_switch(horizon, n)
     
     else:
-        guide_steps = __curriculum(horizon)
+        guide_steps = __curriculum(horizon, n)
 
     # main training loop
-    for guide_step in guide_steps:
+    for i, guide_step in enumerate(guide_steps):
         
+        # getting policy
+        guide_policy = JSRL.guide_policy_return()
+        exploration_policy = JSRL.exploration_policy_return()
+
+        combine_policy = (i + 1)/len(guide_steps) * exploration_policy + (1 - (i + 1)/len(guide_steps)) * guide_policy
+        
+        # setting up
         guided_steps = guide_step
         exploration_steps = horizon - guide_step
+
+        combine_policy = (i + 1)/len(guide_steps) * exploration_policy + (1 - (i + 1)/len(guide_steps)) * guide_policy
+
+        
 
         
         
